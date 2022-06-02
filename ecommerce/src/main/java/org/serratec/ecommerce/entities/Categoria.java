08abc18df113856505209f268f40eae082b2d3d8
gbassan09 <102334775+gbassan09@users.noreply.github.com>
2022-06-02 19:29:53 -0300

    Add files via upload

--- ecommerce/src/main/java/org/serratec/ecommerce/entities/Categoria.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/entities/Categoria.java
@@ -1,4 +1,5 @@
-package org.serratec.ecommerce.entities;
+package org.serratec.ecommerce.entities; //finish
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -6,24 +7,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
 @Table(name = "categoria")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCategoria")
+@JsonIdentityInfo(
+	generator = ObjectIdGenerators.PropertyGenerator.class, 
+	property = "idCategoria")
 public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_categoria")
 	private Integer idCategoria;
 
-	@Column(name = "nome")
+	@Column(name = "nome_categoria")
+	@NotBlank(message = "O nome da categoria não pode ficar em branco.")
 	private String nomeCategoria;
 
 	@Column(name = "descricao")
-	private String descricaoCategoria;
+	private String nomeDescricao;
 
 	public Integer getIdCategoria() {
 		return idCategoria;
@@ -41,12 +46,12 @@ public class Categoria {
 		this.nomeCategoria = nomeCategoria;
 	}
 
-	public String getDescricaoCategoria() {
-		return descricaoCategoria;
+	public String getnomeDescricao() {
+		return nomeDescricao;
 	}
 
-	public void setDescricaoCategoria(String descricaoCategoria) {
-		this.descricaoCategoria = descricaoCategoria;
+	public void setnomeDescricao(String nomeDescricao) {
+		this.nomeDescricao = nomeDescricao;
 	}
 
 }
--- ecommerce/src/main/java/org/serratec/ecommerce/services/CategoriaService.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/services/CategoriaService.java
@@ -1,35 +1,104 @@
 package org.serratec.ecommerce.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.serratec.ecommerce.entities.Categoria;
 import org.serratec.ecommerce.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.serratec.ecomercio.dtos.CategoriaDTO;
+
 
 @Service
 public class CategoriaService {
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
 	public List<Categoria> findAllCategoria() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria findCategoriaById(Integer id) {
 		return categoriaRepository.findById(id).isPresent() ? categoriaRepository.findById(id).get() : null;
+	}
+
+	public CategoriaDTO findCategoriaDTOById(Integer id) {
+		Categoria categoria = categoriaRepository.findById(id).isPresent() ? categoriaRepository.findById(id).get()
+				: null;
+
+		CategoriaDTO categoriaDTO = new CategoriaDTO();
+		if (null != categoria) {
+			categoriaDTO = converterEntidadeParaDto(categoria);
+		}
+		return categoriaDTO;
 	}
 
 	public Categoria saveCategoria(Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
 
+	public CategoriaDTO saveCategoriaDTO(CategoriaDTO categoriaDTO) {
+
+		Categoria categoria = new Categoria();
+
+		categoria.setIdCategoria(categoriaDTO.getIdCategoria());
+		Categoria novaCategoria = categoriaRepository.save(categoria);
+
+		return converterEntidadeParaDto(novaCategoria);
+	}
+
+	public Categoria saveCategoriaComFoto(String categoriaString, MultipartFile file) throws Exception {
+
+		Categoria categoriaConvertida = new Categoria();
+		try {
+			ObjectMapper objMapper = new ObjectMapper();
+			categoriaConvertida = objMapper.readValue(categoriaString, Categoria.class);
+		} catch (IOException e) {
+			System.out.println("Ocorreu um erro na conversão");
+		}
+
+		Categoria categoriaBD = categoriaRepository.save(categoriaConvertida);
+		categoriaBD.setNomeImagem(categoriaBD.getIdCategoria() + "_" + file.getOriginalFilename());
+		Categoria categoriaAtualizada = categoriaRepository.save(categoriaBD);
+
+		try {
+			arquivoService.criarArquivo(categoriaBD.getIdCategoria() + "_" + file.getOriginalFilename(), file);
+		} catch (Exception e) {
+			throw new Exception("Ocorreu um erro ao tentar copiar o arquivo - " + e.getStackTrace());
+		}
+		
+		//cuidado para definir um endereco 
+		String corpoEmail = "Foi cadastrada uma nova categoria" + categoriaAtualizada.toString();
+		emailService.enviarEmailTexto("teste@teste.com", "Cadastro de categoria", corpoEmail);
+
+		return categoriaAtualizada;
+
+	}
+
 	public Categoria updateCategoria(Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
 
-	public void deleteCategoriaById(Integer id) {
-		categoriaRepository.deleteById(id);
+	public void deleteCategoria(Integer id) {
+		Categoria inst = categoriaRepository.findById(id).get();
+		categoriaRepository.delete(inst);
 	}
 
+	public void deleteCategoria(Categoria categoria) {
+		categoriaRepository.delete(categoria);
+	}
+
+	private CategoriaDTO converterEntidadeParaDto(Categoria categoria) {
+		CategoriaDTO categoriaDTO = new CategoriaDTO();
+
+		categoriaDTO.setIdCategoria(categoria.getIdCategoria());
+		categoriaDTO.setNomeCategoria(categoria.getNomeCategoria());
+
+		return categoriaDTO;
+	}
 }
+
