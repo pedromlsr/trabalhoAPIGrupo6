c6061d7a5046eb2b4ad477efe96688a8bf08c4cb
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-06-03 12:19:23 -0300

    Testes para delete e findall no itemPedido

--- ecommerce/src/main/java/org/serratec/ecommerce/controllers/ItemPedidoController.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/controllers/ItemPedidoController.java
@@ -2,17 +2,13 @@ package org.serratec.ecommerce.controllers;
 
 import java.util.List;
 
-import org.serratec.ecommerce.entities.ItemPedido;
+import org.serratec.ecommerce.dtos.ItemPedidoDTO;
+import org.serratec.ecommerce.exceptions.NoSuchElementFoundException;
 import org.serratec.ecommerce.services.ItemPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +18,13 @@ public class ItemPedidoController {
 	@Autowired
 	ItemPedidoService itemPedidoService;
 
-//	@GetMapping
-//	public ResponseEntity<List<ItemPedido>> findAllItemPedido() {
-//		List<ItemPedido> itemPedidoList = itemPedidoService.findAllItemPedido();
-//		return new ResponseEntity<>(itemPedidoList, HttpStatus.OK);
-//	}
+	@GetMapping
+	public ResponseEntity<List<ItemPedidoDTO>> findAllItemPedidoDTO() {
+		if (itemPedidoService.findAllItemPedidoDTO() == null) {
+			throw new NoSuchElementFoundException("Nenhum itemPedido encontrado.");
+		}
+		return new ResponseEntity<>(itemPedidoService.findAllItemPedidoDTO(), HttpStatus.OK);
+	}
 //
 //	@GetMapping("/{id}")
 //	public ResponseEntity<ItemPedido> findItemPedidoById(@PathVariable Integer id) {
--- ecommerce/src/main/java/org/serratec/ecommerce/exceptions/CustomExceptionHandler.java
+++ ecommerce/src/main/java/org/serratec/ecommerce/exceptions/CustomExceptionHandler.java
@@ -75,17 +75,17 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
 	
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	protected Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		
-		return errors;
-	}
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	@ExceptionHandler(MethodArgumentNotValidException.class)
+//	protected Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+//		Map<String, String> errors = new HashMap<>();
+//		
+//		ex.getBindingResult().getAllErrors().forEach((error) -> {
+//			String fieldName = ((FieldError) error).getField();
+//			String errorMessage = error.getDefaultMessage();
+//			errors.put(fieldName, errorMessage);
+//		});
+//		
+//		return errors;
+//	}
 }
