22826813946474270f04536a601e44649b358e98
Larissa Araujo <lariarau11@gmail.com>
2022-06-03 23:18:56 -0300

    Atualizando validações

---
+++ ecommerce/src/main/java/org/serratec/ecommerce/dtos/EnderecoDTO.java
@@ -0,0 +1,68 @@
+package org.serratec.ecommerce.dtos;
+
+public class EnderecoDTO {
+	private Integer idEndereco;
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String numero;
+	private String complemento;
+
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+}
---
+++ ecommerce/src/main/java/org/serratec/ecommerce/dtos/ProdutoPostDTO.java
@@ -0,0 +1,58 @@
+package org.serratec.ecommerce.dtos;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class ProdutoPostDTO {
+
+	private Integer idProduto;
+	
+	@NotBlank(message="O nome do produto não pode estar em branco")
+	private String nomeProduto;
+	
+	@NotBlank(message="A descrição do produto não pode estar em branco")
+	private String descricaoProduto;
+	
+	@NotNull(message="A quantidade de produto em estoque não pode estar em branco")
+	private Integer qtdEstoque;	
+	
+	@NotNull(message="O valor do produto não pode estar em branco")
+	private Double valorUnitario;
+		
+	//private CategoriaDTO idCategoria;
+	
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}	
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	
+	
+
+}
